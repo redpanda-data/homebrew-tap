5ce52e8b7152c538991d2cbfa48061aec1181bb3
Roko <rkruze@gmail.com>
2021-03-24 16:08:26 -0700

    redpanda 21.3.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/redpanda.rb
+++ Formula/redpanda.rb
@@ -1,9 +1,9 @@
 class Redpanda < Formula
     desc "Fastest Queue in the West"
     homepage "https://www.vectorized.io"
-    url "https://github.com/vectorizedio/redpanda/releases/download/v21.3.4/rpk-darwin-amd64.zip"
-    version "21.3.4"
-    sha256 "38f4a9894bb1e2a8c1bba8c973cb61470eaa453628c9edd66933cb46737c8d4b"
+    url "https://github.com/vectorizedio/redpanda/releases/download/v21.3.5/rpk-darwin-amd64.zip"
+    version "21.3.5"
+    sha256 "3031d01360263387deb369acd24c7d611d587442ea5605d2e3b85925480de0b1"
 
     bottle :unneeded
 
